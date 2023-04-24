61c416c7eb7a0c62dc2ddb99324c96ad65d585da
Fabian <fabiankanefke@web.de>
2023-04-24 16:53:15 +0200

    Added new key combination. Now `SHIFT + Tab` allows the user to move the list upwards. Changed README.md.

--- Symbol_Mapper_Project/Components/Searchbox.xaml.cs
+++ Symbol_Mapper_Project/Components/Searchbox.xaml.cs
@@ -1,6 +1,7 @@
 // Copyright (c) Microsoft Corporation and Contributors.
 // Licensed under the MIT License.
 
+using Microsoft.UI.Input;
 using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
 using Microsoft.UI.Xaml.Input;
@@ -8,6 +9,7 @@ using Symbol_Mapper_Project.Models;
 using System;
 using System.Collections;
 using Windows.System;
+using Windows.UI.Core;
 
 namespace Symbol_Mapper_Project.Components
 {
@@ -58,104 +60,119 @@ namespace Symbol_Mapper_Project.Components
 
         private void OnPreviewKeyDown(object sender, KeyRoutedEventArgs e)
         {
+            // Get shift and tab state to use the key combination `SHIFT + TAB` to move the list up
+            CoreVirtualKeyStates shift_state = InputKeyboardSource.GetKeyStateForCurrentThread(VirtualKey.Shift);
+            bool shift_down = shift_state == CoreVirtualKeyStates.Down || shift_state == (CoreVirtualKeyStates.Down | CoreVirtualKeyStates.Locked);
+
+            CoreVirtualKeyStates tab_state = InputKeyboardSource.GetKeyStateForCurrentThread(VirtualKey.Tab);
+            bool tab_down = tab_state == CoreVirtualKeyStates.Down || tab_state == (CoreVirtualKeyStates.Down | CoreVirtualKeyStates.Locked);
+            
             // Implement keyboard movement
             // and don't allow one SPACE as the first character
-            switch (e.Key)
+            if (e.Key == VirtualKey.Up ||
+               (tab_down &&
+                shift_down))
             {
-                case VirtualKey.Down:
-                case VirtualKey.Tab:
-                    {
-                        e.Handled = true;
-
-                        int last_index = search_display.SelectedIndex;
-
-                        if (last_index == -1)
-                        {
-                            searchbox_last_value = searchbox.Text;
-                        }
-
-                        int list_len = (search_display.ItemsSource as IList).Count;
-
-                        if (list_len > 0)
-                        {
-                            int next_index = (last_index + 1) % list_len;
-
-                            search_display.SelectedIndex = next_index;
-
-                            searchbox.Text = (search_display.SelectedItem as UnicodeData).UnicodeCharacter;
-                            search_display.ScrollIntoView(search_display.SelectedItem);
-
-                            if (next_index == 0 && (last_index + 1) > 0)
-                            {
-                                search_display.SelectedIndex = -1;
-                                searchbox.Text = searchbox_last_value;
-                            }
-                        }
-                        else
-                        {
-                            search_display.SelectedIndex = -1;
-                        }
-
-                        searchbox.SelectionStart = searchbox.Text.Length;
-                        searchbox.SelectionLength = 0;
-
-                        break;
-                    }
-
-                case VirtualKey.Up:
-                    {
-                        int last_index = search_display.SelectedIndex;
-                        int next_index = last_index - 1;
-
-                        if (search_display.SelectedIndex == 0)
-                        {
-                            search_display.SelectedIndex = -1;
-
-                            searchbox.Text = searchbox_last_value;
-                        }
-                        else if (next_index < -1)
-                        {
-                            int list_len = (search_display.ItemsSource as IList).Count;
-
-                            search_display.SelectedIndex = list_len - 1;
-                            search_display.ScrollIntoView(search_display.SelectedItem);
-
-                            searchbox.Text = (search_display.SelectedItem as UnicodeData).UnicodeCharacter;
-                        }
-                        else
-                        {
-                            search_display.SelectedIndex = next_index;
-                            search_display.ScrollIntoView(search_display.SelectedItem);
-
-                            searchbox.Text = (search_display.SelectedItem as UnicodeData).UnicodeCharacter;
-                        }
-
-                        searchbox.SelectionStart = searchbox.Text.Length;
-                        searchbox.SelectionLength = 0;
-
-                        break;
-                    }
-
-                case VirtualKey.Space:
-                    if (searchbox.Text.Length == 0)
-                    {
-                        e.Handled = true;
-                    }
-                    break;
-
-                case VirtualKey.Enter:
-                    SearchboxQuerySubmittedEventArgs args = new()
-                    {
-                        SelectedItem = search_display.SelectedItem
-                    };
-
-                    OnQuerySubmitted(args);
-                    break;
-
-                default:
-                    search_display.SelectedIndex = -1;
-                    break;
+                e.Handled = true;
+                MoveListUp();
             }
+            else if (e.Key == VirtualKey.Down ||
+                    (tab_down &&
+                    !shift_down))
+            {
+                e.Handled = true;
+                MoveListDown();
+            }
+            else if (e.Key == VirtualKey.Space &&
+                     searchbox.Text.Length == 0)
+            {
+                e.Handled = true;
+            }
+            else if (e.Key == VirtualKey.Enter)
+            {
+                e.Handled = true;
+                SearchboxQuerySubmittedEventArgs args = new()
+                {
+                    SelectedItem = search_display.SelectedItem
+                };
+
+                OnQuerySubmitted(args);
+            }
+            else if (e.Key == VirtualKey.Shift)
+            {
+                e.Handled = true;
+            }
+            else
+            {
+                search_display.SelectedIndex = -1;
+            }
+        }
+        
+        private void MoveListDown()
+        {
+            int last_index = search_display.SelectedIndex;
+
+            if (last_index == -1)
+            {
+                searchbox_last_value = searchbox.Text;
+            }
+
+            int list_len = (search_display.ItemsSource as IList).Count;
+
+            if (list_len > 0)
+            {
+                int next_index = (last_index + 1) % list_len;
+
+                search_display.SelectedIndex = next_index;
+
+                searchbox.Text = (search_display.SelectedItem as UnicodeData).UnicodeCharacter;
+                search_display.ScrollIntoView(search_display.SelectedItem);
+
+                if (next_index == 0 && (last_index + 1) > 0)
+                {
+                    search_display.SelectedIndex = -1;
+                    searchbox.Text = searchbox_last_value;
+                }
+            }
+            else
+            {
+                search_display.SelectedIndex = -1;
+            }
+
+            searchbox.SelectionStart = searchbox.Text.Length;
+            searchbox.SelectionLength = 0;
+        }
+
+        private void MoveListUp()
+        {
+            int last_index = search_display.SelectedIndex;
+            int next_index = last_index - 1;
+
+            if (search_display.SelectedIndex == 0)
+            {
+                search_display.SelectedIndex = -1;
+
+                searchbox.Text = searchbox_last_value;
+            }
+            else if (next_index < -1)
+            {
+                int list_len = (search_display.ItemsSource as IList).Count;
+
+                search_display.SelectedIndex = list_len - 1;
+                search_display.ScrollIntoView(search_display.SelectedItem);
+
+                searchbox.Text = (search_display.SelectedItem as UnicodeData).UnicodeCharacter;
+            }
+            else
+            {
+                search_display.SelectedIndex = next_index;
+                search_display.ScrollIntoView(search_display.SelectedItem);
+
+                searchbox.Text = (search_display.SelectedItem as UnicodeData).UnicodeCharacter;
+            }
+
+            searchbox.SelectionStart = searchbox.Text.Length;
+            searchbox.SelectionLength = 0;
         }
 
         private void OnTextChanged(object _, TextChangedEventArgs e)
